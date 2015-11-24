1dc7eb805c27de00a7b1847f6bb86f5525dc07d2
Ramki <wpdev.ramki@gmail.com>
2015-11-24 14:05:14 +0530

    third menu err fixed

--- base.php
+++ base.php
@@ -29,7 +29,7 @@
   ?>
 
     <?php if (has_nav_menu('third_navigation')) : ?>
-  <section id="cat_nav" class="navclass"><!-- Start Menu Section -->
+  <section id="cat_nav" class="navclass third"><!-- Start Menu Section -->
     <div class="container menu">
      <nav id="nav-third" class="clearfix" role="navigation">
      <?php wp_nav_menu(array('theme_location' => 'third_navigation', 'menu_class' => 'sf-menu')); ?>
