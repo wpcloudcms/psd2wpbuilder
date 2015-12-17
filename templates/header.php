40f3495a67a1e2360ba20edad1ff176851642299
Ramki <wpdev.ramki@gmail.com>
2015-12-17 17:41:41 +0530

    featured image at header

--- templates/header.php
+++ templates/header.php
@@ -139,5 +139,13 @@
 </div>
           <?php } ?>
     </div></div>
+    <div class="featured container">
+<?php if (has_post_thumbnail( $post->ID ) ): ?>
+  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+	<img src="<?php echo $image[0]; ?>" alt="" />
+<?php else:
+	// dynamic_sidebar( 'top-slider-homeonly' );
+endif; ?>
+</div> 
         <?php do_action('kt_after_header_content'); ?>
 </header>
