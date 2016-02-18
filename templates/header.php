2c6bb8054762e570abc72f0fe3d858444778633e
Ramki <wpdev.ramki@gmail.com>
2016-02-18 22:53:24 +0530

    err fix featured image

--- templates/header.php
+++ templates/header.php
@@ -91,7 +91,7 @@
                 if(is_active_sidebar('slider-above-widget')) { dynamic_sidebar('slider-above-widget'); } ?>
               </div>
                <?php } ?>
-    <div class="featured container">
+    <div class="featured container box">
 <?php if (has_post_thumbnail( $post->ID ) ): ?>
   <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 	<img src="<?php echo $image[0]; ?>" alt="" />
