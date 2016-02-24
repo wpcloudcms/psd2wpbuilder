59eb0d802192972ba5b70171056765c4322000cb
Ramki <wpdev.ramki@gmail.com>
2016-02-25 00:29:40 +0530

    featured err fix

--- templates/header.php
+++ templates/header.php
@@ -87,7 +87,8 @@
                 if(is_active_sidebar('slider-above-widget')) { dynamic_sidebar('slider-above-widget'); } ?>
               </div>
                <?php } ?>
-    <div class="featured container box">
+  <div class="container box">
+    <div class="featured row">
 <?php if (has_post_thumbnail( $post->ID ) ): ?>
   <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 	<img src="<?php echo $image[0]; ?>" alt="" />
@@ -95,6 +96,7 @@
 	// dynamic_sidebar( 'top-slider-homeonly' );
 endif; ?>
 </div> 
+ </div>
    <?php if(isset($virtue_premium['slider_box_layout']) and $virtue_premium['slider_box_layout'] == 'sliderwide') { ?>
   <div class="container-fluid">
 <?php } else { ?>
