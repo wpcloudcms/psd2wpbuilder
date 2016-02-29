ce219f86a09577535e59ac0817de70b7fc51f92f
Ramki <wpdev.ramki@gmail.com>
2016-02-29 16:10:44 +0530

    err fix

--- themeoptions/options.php
+++ themeoptions/options.php
@@ -155,7 +155,7 @@ Redux::setSection( $opt_name, array(
             'subtitle' => __('Select Sidebar Layout Style', 'virtue'),
             'options' => array(
                     'onlyleftsidebar' => array('alt' => 'only Left Sidebar', 'img' => get_stylesheet_directory_uri().'/themeoptions/options/img/sidebar/only-left.png'),
-                    'onlyrightsidebar' => array('alt' => 'only Right Sidebar', 'img' => get_stylesheet_directory_uri().'/themeoptions/options/img/sidebar/only-left.png'),
+                    'onlyrightsidebar' => array('alt' => 'only Right Sidebar', 'img' => get_stylesheet_directory_uri().'/themeoptions/options/img/sidebar/only-right.png'),
                     'bothsidebar' => array('alt' => 'Both Sidebar', 'img' => get_stylesheet_directory_uri().'/themeoptions/options/img/sidebar/both-sides.png'),
                 ),
             'default' => 'onlyrightsidebar',
