d86f6aec85ab2d71e5c5333bfb3a00de6a7f7bbd
Ramki <wpdev.ramki@gmail.com>
2016-03-01 15:15:54 +0530

    err fix

--- templates/header.php
+++ templates/header.php
@@ -95,7 +95,7 @@ endif; ?>
    <?php if(isset($virtue_premium['slider_box_layout']) and $virtue_premium['slider_box_layout'] == 'sliderwide') { ?>
   <div class="full-slider">
 <?php } else { ?>
- <div class="container">
+ <div class="container slider-banner">
 <?php } ?>
        <?php if(isset($virtue_premium['slider_widget_layout']) && $virtue_premium['slider_widget_layout'] == 'slideronly') { ?>
     <div class="col-md-12">
