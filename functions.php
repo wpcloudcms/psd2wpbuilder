49c17d88d8f951164f7e33779ab63167ba79cce1
BigCloudCMS <support@bigcloudcms.com>
2016-03-09 22:43:28 +0530

    er fix

--- functions.php
+++ functions.php
@@ -14,15 +14,15 @@ function custom_scripts() {
     wp_register_style( 'wpcloudcms-css', get_stylesheet_directory_uri().'/assets/css/wpcloudcms.css' );
     wp_enqueue_style('wpcloudcms-css');
     
-//    wp_register_style( 'animate-css', get_stylesheet_directory_uri().'/assets/css/animate.css' );
-//    wp_enqueue_style('animate-css');
+    wp_register_style( 'animate-css', get_stylesheet_directory_uri().'/assets/css/animate.css' );
+    wp_enqueue_style('animate-css');
     
-//    wp_register_style( 'custom-css', get_stylesheet_directory_uri().'/custom.css' );
-//    wp_enqueue_style('custom-css');
+    wp_register_style( 'custom-css', get_stylesheet_directory_uri().'/custom.css' );
+    wp_enqueue_style('custom-css');
     
-//    wp_register_script( 'custom-js', get_stylesheet_directory_uri() . '/custom.js');
-//    wp_enqueue_script('custom-js');
-//    
+    wp_register_script( 'custom-js', get_stylesheet_directory_uri() . '/custom.js');
+    wp_enqueue_script('custom-js');
+    
 
 	}
 
