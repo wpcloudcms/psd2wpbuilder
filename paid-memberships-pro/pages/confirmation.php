2e7b1fbd6dd9bad6106f3edaf3a0bb0da2d74fab
BigCloudCMS <support@bigcloudcms.com>
2016-03-21 23:47:52 +0530

    err fix

--- paid-memberships-pro/pages/confirmation.php
+++ paid-memberships-pro/pages/confirmation.php
@@ -25,7 +25,7 @@
 		$pmpro_invoice->getUser();
 		$pmpro_invoice->getMembershipLevel();			
 				
-		$confirmation_message .= "<p>" . sprintf(__('Below are details about your membership account and a receipt for your initial membership invoice. A welcome email with a copy of your initial membership invoice has been sent to %s.', 'pmpro'), $pmpro_invoice->user->user_email) . "</p>";
+		$confirmation_message .= "<p>" . sprintf(__('A welcome email with a copy of your initial membership invoice has been sent to %s.', 'pmpro'), $pmpro_invoice->user->user_email) . "</p>";
 		
 		//check instructions		
 		if($pmpro_invoice->gateway == "check" && !pmpro_isLevelFree($pmpro_invoice->membership_level))
