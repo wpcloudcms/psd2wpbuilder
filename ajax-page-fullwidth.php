19fc6a59694269429df2558c5f115afd09916688
BigCloudCMS <support@bigcloudcms.com>
2016-03-24 10:40:17 +0530

    ajaxpage full width

---
+++ ajax-page-fullwidth.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Fullwidth Ajax Page
+*/
+?>
+
+	
+        <div id="content">
+     <div class="container">
+   		<div class="row">    
+            <div class="wrap clearfix contentclass hfeed" role="document"> 
+     		<div class="main <?php echo kadence_main_class(); ?>" id="ktmain" role="main">
+				<div class="entry-content ajaxpage" itemprop="mainContentOfPage">
+					<?php get_template_part('templates/content', 'page'); ?>
+				</div>
+	</div><!-- /.main -->
