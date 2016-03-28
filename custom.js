345067d0cf759de623505f7dd9f12ff85640fe85
BigCloudCMS <support@bigcloudcms.com>
2016-03-28 13:33:29 +0530

    fix

--- custom.js
+++ custom.js
@@ -4,11 +4,15 @@
         $(".toggle2").hide();
         $(".toggle2").addClass("hidden");
         $(".toggle1").removeClass("hidden");
+        $(".monthly.billing").addClass("black");
+        $(".monthly.billing").removeClass("black");
     });
     $("#toggle2").click(function(){
         $(".toggle2").show();
         $(".toggle1").hide();
         $(".toggle1").addClass("hidden");
         $(".toggle2").removeClass("hidden");
+        $(".annual.billing").addClass("black");
+        $(".annual.billing").removeClass("black");
     });
 });
