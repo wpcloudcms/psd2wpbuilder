6d5d032a7d6d65d44e851b8ba043fdd94b4cccb6
BigCloudCMS <support@bigcloudcms.com>
2016-04-01 19:14:12 +0530

    err fix

--- functions.php
+++ functions.php
@@ -21,15 +21,15 @@ function custom_scripts() {
     wp_enqueue_style('animate-css');
     
     wp_enqueue_script( 'js-lib-1', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js');
+           
+    wp_register_script( 'scrollSpeed-js', get_stylesheet_directory_uri() . '/assets/js/jQuery.scrollSpeed.js');  
+    wp_enqueue_script('scrollSpeed-js');
     
 //  wp_enqueue_script( 'js-lib-2', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js');
     
     wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' );
     
      wp_enqueue_script( 'waypoints-js', 'https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.5/waypoints.min.js');
-    
-    wp_register_script( 'scrollSpeed-js', get_stylesheet_directory_uri() . '/assets/js/jQuery.scrollSpeed.js');  
-    wp_enqueue_script('scrollSpeed-js');
     
     wp_register_script( 'custom-js', get_stylesheet_directory_uri() . '/custom.js');  
     wp_enqueue_script('custom-js');
