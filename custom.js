0b93019b6106042728b8d26a9ae1bed45a71805f
BigCloudCMS <support@bigcloudcms.com>
2016-04-05 14:05:44 +0530

    fix

--- custom.js
+++ custom.js
@@ -5,7 +5,7 @@ $(".loader").css("opacity", 0), setTimeout(function() {
     $("nav#nav-main").css({"display" : "block"});
     $("i.beforelogin").closest("li").addClass("beforelogin");
     $("i.afterlogin").closest("li").addClass("afterlogin");
-    $("nav#nav-main ul li a span").insertAfter("::after");
+    $("::after").insertAfter("nav#nav-main ul li a span");
 });
 
 $(document).ready(function(){
